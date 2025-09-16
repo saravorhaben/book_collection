7a5e1d5c3d46547b30a033c6ae4255c6c8b71517
Sara <134113685+saravorhaben@users.noreply.github.com>
2025-09-15 22:05:49 -0500

    added basics

--- Gemfile
+++ Gemfile
@@ -1,8 +1,12 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.1.2"
+ruby "3.4.5"
 gem 'concurrent-ruby', '1.3.4' 
+gem "bigdecimal"
+gem "mutex_m"
+
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 
@@ -56,6 +60,8 @@ end
 group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
+  gem 'webdrivers'
+  gem 'simplecov', :require => false
 end
 
 group :development do
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -22,15 +22,15 @@ RSpec.describe "Books", type: :system do
   end
   it 'A new book with author and title is being added' do
     visit 'books/new'
-    fill_in 'Title', with: 'me'
-    fill_in 'Author', with: 'me'
+    # fill_in 'Title', with: 'me'
+    # fill_in 'Author', with: 'me'
     fill_in 'Price', with: '9'
     click_on 'Create Book'
     expect(page).to have_content('Book was successfully created.')
   end
   it 'A new book with title and price is being added' do
     visit 'books/new'
-    fill_in 'Title', with: 'me'
+    #fill_in 'Title', with: 'me'
     fill_in 'Author', with: 'me'
     fill_in 'Price', with: '7'
     
@@ -40,7 +40,7 @@ RSpec.describe "Books", type: :system do
     it 'A new book with title and Publish Date is being added' do
     visit 'books/new'
     fill_in 'Title', with: 'meow'
-    fill_in 'Author', with: 'me'
+    # fill_in 'Author', with: 'me'
     fill_in 'Published date', with: "2025-01-12"
     fill_in 'Price', with: '9'
     click_on 'Create Book'
@@ -51,6 +51,7 @@ RSpec.describe "Books", type: :system do
    it 'A new book with price but no author is being added' do
     visit 'books/new'
     fill_in 'Title', with: 'lol'
+    fill_in 'Author', with: 'me'
     fill_in 'Price', with: '9'
     click_on 'Create Book'
     expect(page).to have_content('Book could not be created')
@@ -60,7 +61,7 @@ RSpec.describe "Books", type: :system do
     visit 'books/new'
     fill_in 'Title', with: 'special'
     fill_in 'Author', with: 'me'
-    fill_in 'Price', with: 'A'
+    fill_in 'Price', with: '9'
     click_on 'Create Book'
     expect(page).to have_content('Book could not be created')
   end
@@ -70,17 +71,17 @@ RSpec.describe "Books", type: :system do
     fill_in 'Title', with: 'no'
     fill_in 'Author', with: 'oof'
     fill_in 'Price', with: '9'
-    fill_in 'Published date', with: "2026-09-12"
+    fill_in 'Published date', with: "2025-1-12"
     click_on 'Create Book'
     expect(page).to have_content('Book could not be created')
   end
 
    it 'A new book with All correct formatting is being added' do
     visit 'books/new'
-    fill_in 'Title', with: 'Gold'
+    # fill_in 'Title', with: 'Gold'
     fill_in 'Author', with: 'me'
     fill_in 'Price', with: '9'
-    fill_in "Published date", with: "2025-01-12"
+    fill_in "Published date", with: "2025-10-12"
     click_on 'Create Book'
     expect(page).to have_content('Book was successfully created.')
   end
